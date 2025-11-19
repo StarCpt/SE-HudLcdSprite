3f745e5364312f89e80c5bc99ce952df3eda4516
StarCpt <86216339+StarCpt@users.noreply.github.com>
2025-11-19 12:01:37 -0800

    reset tag + tag token system for better flexibility

--- HudSprite/Plugin.cs
+++ HudSprite/Plugin.cs
@@ -245,6 +245,34 @@ public class Plugin : IPlugin
             return double.TryParse(str, out var val) ? val : defaultValue;
         }
 
+        private enum TagType
+        {
+            LineStart = 0,
+            ColorStart = 1,
+            ColorEnd = 2,
+            Reset = 3,
+        }
+
+        const string COLOR_TAG_START = "<color=";
+        const char COLOR_TAG_END = '>';
+        const string RESET_TAG = "<reset>";
+
+        private struct TagToken(TagType type, int startIndex)
+        {
+            public TagType Type = type;
+            public int StartIndex = startIndex;
+            public readonly int EndIndex => StartIndex + Type switch
+            {
+                TagType.ColorStart => COLOR_TAG_START.Length,
+                TagType.ColorEnd => 1,
+                TagType.Reset => RESET_TAG.Length,
+                _ => throw new Exception(),
+            };
+        }
+
+        // reused globally to reduce allocations
+        private static readonly List<TagToken> _tempTokens = [];
+
         /// <summary>
         /// 
         /// </summary>
@@ -284,7 +312,7 @@ public class Plugin : IPlugin
 
             if (Comp.ContentType is ContentType.TEXT_AND_IMAGE)
             {
-                var screenSize = MyGuiManager.GetFullscreenRectangle();
+                var screenWidth = MyGuiManager.GetFullscreenRectangle().Width;
                 var font = MyFontDefinition.GetFont(Comp.Font.SubtypeId);
 
                 string[] lines = Comp.Text.Split('\n');
@@ -303,86 +331,134 @@ public class Plugin : IPlugin
                         offset = new Vector2(0.0001f, -0.005f + (0.0229f * l)) * Scale;
                     }
 
-                    var line = lines[l];
-                    const string COLOR_TAG_START = "<color=";
-                    const char COLOR_TAG_END = '>';
-
-                    float xOffset = 0;
-
-                    string[] candidates = line.Split([COLOR_TAG_START], StringSplitOptions.None);
-                    Color prevColor = TextColor;
-                    foreach (var candidate in candidates)
+                    if (TopLeft.X + offset.X >= 1 || TopLeft.Y + offset.Y >= 1)
                     {
-                        int end = candidate.IndexOf(COLOR_TAG_END);
-                        Color color = default;
-                        bool colorValid = false;
-                        if (end >= 0)
+                        break; // early out for nonvisible text
+                    }
+
+                    var line = lines[l];
+
+                    // identify tags
+                    int i = 0;
+                    _tempTokens.Clear();
+                    List<TagToken> tokens = _tempTokens;
+                    tokens.Add(new TagToken(TagType.LineStart, 0));
+                    while (i < line.Length)
+                    {
+                        if (line[i] is '<')
                         {
-                            string colorName = candidate.Substring(0, end);
-                            if (_colorByName.TryGetValue(colorName, out color))
+                            if (line.Length >= i + COLOR_TAG_START.Length && line.IndexOf(COLOR_TAG_START, i, COLOR_TAG_START.Length) == i)
                             {
-                                colorValid = true;
+                                tokens.Add(new TagToken(TagType.ColorStart, i));
+                                i += COLOR_TAG_START.Length;
+                            }
+                            else if (line.Length >= i + RESET_TAG.Length && line.IndexOf(RESET_TAG, i, RESET_TAG.Length) == i)
+                            {
+                                tokens.Add(new TagToken(TagType.Reset, i));
+                                i += RESET_TAG.Length;
                             }
                             else
                             {
-                                string[] args = colorName.Split([','], 4);
-                                if (args.Length == 3)
-                                {
-                                    // only rgb
-                                    if (byte.TryParse(args[0], out byte r) &&
-                                        byte.TryParse(args[1], out byte g) &&
-                                        byte.TryParse(args[2], out byte b))
-                                    {
-                                        color = new Color(r, g, b);
-                                        colorValid = true;
-                                    }
-                                }
-                                else if (args.Length == 4)
-                                {
-                                    // rgba
-                                    if (byte.TryParse(args[0], out byte r) &&
-                                        byte.TryParse(args[1], out byte g) &&
-                                        byte.TryParse(args[2], out byte b) &&
-                                        byte.TryParse(args[3], out byte a))
-                                    {
-                                        color = new Color(r, g, b, a);
-                                        colorValid = true;
-                                    }
-                                }
+                                i++;
                             }
                         }
-
-                        if (!colorValid)
+                        else if (line[i] is '>' && tokens.Count > 0 && tokens[tokens.Count - 1].Type is TagType.ColorStart)
                         {
-                            color = prevColor;
-                        }
-                        prevColor = color;
-
-                        string textToDraw;
-                        if (!colorValid || end <= 0)
-                        {
-                            // invalid or malformed color tag
-                            textToDraw = xOffset is 0 ? candidate : $"{COLOR_TAG_START}{candidate}";
+                            tokens.Add(new TagToken(TagType.ColorEnd, i));
+                            i++;
                         }
                         else
                         {
-                            if (candidate.Length - (end + 1) <= 0)
+                            i++;
+                        }
+                    }
+
+                    if (tokens.Count > 1) // always has at least 1 LineStart token
+                    {
+                        float xOffset = 0;
+                        Color color = TextColor;
+
+                        int prevSegmentEndIndex = 0;
+                        for (int t = 0; t < tokens.Count; t++)
+                        {
+                            TagToken token = tokens[t];
+                            int segmentStart = prevSegmentEndIndex;
+                            int segmentEnd = (t + 1) < tokens.Count ? tokens[t + 1].StartIndex : line.Length;
+                            if (token.Type is TagType.ColorStart && t + 1 < tokens.Count)
                             {
-                                // valid color tag but no text to draw after the closing char
-                                continue;
+                                TagToken nextToken = tokens[t + 1];
+                                if (nextToken.Type is TagType.ColorEnd)
+                                {
+                                    if (TryParseColor(line.Substring(token.EndIndex, nextToken.StartIndex - token.EndIndex), out Color newColor))
+                                    {
+                                        color = newColor;
+                                        segmentStart = nextToken.EndIndex;
+                                    }
+                                    t++;
+                                    segmentEnd = (t + 1) < tokens.Count ? tokens[t + 1].StartIndex : line.Length;
+                                }
+                            }
+                            else if (token.Type is TagType.Reset)
+                            {
+                                color = TextColor;
+                                segmentStart = token.EndIndex;
                             }
 
-                            textToDraw = candidate.Substring(end + 1);
-                        }
+                            string text = line.Substring(segmentStart, segmentEnd - segmentStart);
+                            if (text.Length > 0)
+                            {
+                                Vector2 pos = TopLeft + offset + new Vector2(xOffset, 0);
+                                DrawString(text, pos, scale, color);
+                                xOffset += GetStringWidth(text, font, scale, screenWidth);
+                            }
 
-                        Vector2 pos = TopLeft + offset + new Vector2(xOffset, 0);
-                        DrawString(textToDraw, pos, scale, color);
-                        xOffset += GetStringWidth(textToDraw, font, scale, screenSize.Width);
+                            prevSegmentEndIndex = segmentEnd;
+                        }
+                    }
+                    else
+                    {
+                        DrawString(line, TopLeft + offset, scale, TextColor);
                     }
                 }
             }
 
             return true;
+        }
+
+        private static bool TryParseColor(string str, out Color color)
+        {
+            if (_colorByName.TryGetValue(str, out color))
+            {
+                return true;
+            }
+            else
+            {
+                string[] args = str.Split([','], 4);
+                if (args.Length == 3)
+                {
+                    // only rgb
+                    if (byte.TryParse(args[0], out byte r) &&
+                        byte.TryParse(args[1], out byte g) &&
+                        byte.TryParse(args[2], out byte b))
+                    {
+                        color = new Color(r, g, b);
+                        return true;
+                    }
+                }
+                else if (args.Length == 4)
+                {
+                    // rgba
+                    if (byte.TryParse(args[0], out byte r) &&
+                        byte.TryParse(args[1], out byte g) &&
+                        byte.TryParse(args[2], out byte b) &&
+                        byte.TryParse(args[3], out byte a))
+                    {
+                        color = new Color(r, g, b, a);
+                        return true;
+                    }
+                }
+            }
+            return false;
         }
 
         // use this instead of MyGuiManager.DrawString() to avoid snapping positions to pixels
